3397ef9e1f33d762069716e92625db9d7d1ea8db
sa4812 <sa4812@2P48_12>
2017-10-07 17:43:15 -0300

    ijiuiu

---
+++ Empresa.Repositorios.SqlServer/EmpresaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Empresa.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Empresa.Repositorios.SqlServer
+{
+    public class EmpresaDbContext : DbContext
+    {
+        public EmpresaDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
